b1ef45f06b3588ead8fe2df9befaf187d075caed
George Papeorgiou <george@Dias.home>
2023-09-23 18:47:03 +0300

    Created Navbar

--- wsfrontend/src/App.js
+++ wsfrontend/src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
