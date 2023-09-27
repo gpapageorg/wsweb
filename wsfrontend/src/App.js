632c87bbdd9f4d089ef4d0b6761f0202b3a3c65d
George Papeorgiou <george@Dias.home>
2023-09-27 20:00:02 +0300

    Deleted old files

--- wsfrontend/src/App.js
+++ wsfrontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import UnderBar from './Components/Navbar/UnderBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <UnderBar />
         <Routes>
           <Route path='/' />
         </Routes>
