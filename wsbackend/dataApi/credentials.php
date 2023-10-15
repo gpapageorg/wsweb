86fd89fd49b3336b98c98727508f558a3c870dc7
George Papeorgiou <george@Dias.home>
2023-10-15 22:16:24 +0300

    Now getting values from url

---
+++ wsbackend/dataApi/credentials.php
@@ -0,0 +1,8 @@
+<?php
+    $credentials = new stdClass();
+
+    $credentials -> servername = "192.168.2.110";
+    $credentials -> username = "root";
+    $credentials -> password = "root";
+    $credentials -> db = 'wsweb';
+?>
--- wsbackend/dataApi/index.php
+++ wsbackend/dataApi/index.php
@@ -1,32 +1,14 @@
 <?php
-    $credentials = new stdClass();
-
-    $credentials -> servername = "192.168.2.110";
-    $credentials -> username = "root";
-    $credentials -> password = "root";
-    $credentials -> db = 'wsweb';
+    include('credentials.php');
 
 
     $conn = initiateConnection($credentials);
 
-$station = new stdClass();
+    $station = initializeStationObj();
+    echo $station->soil;
+    
+   getValues($station); //http://localhost/api/index.php?loc=patras&name=patrasin&id=5&temp=25&humidity=23&press=1013&altitude=10
 
-
-// $station -> name = $_GET["name"]; 
-// $station -> id = $_GET["id"];
-// $station -> temp = $_GET["temp"];
-// $station -> hum = $_GET["humidity"];
-// $station -> press = $_GET["press"];
-// $station -> alt = $_GET["altitude"];
-// $station -> soil = $_GET["soil"];
-$station -> location = "patras";
-$station -> name = "kostas" ;
-$station -> id = 0;
-$station -> temp = 40;
-$station -> hum = 67;
-$station -> press = 577;
-$station -> alt = 8;
-$station -> soil = 8;
 
 insertIntoDb($conn,$station);
 
@@ -43,7 +25,8 @@ function initiateConnection($credentials)
 }
 function insertIntoDb($conn, $station)
 {
-    $sqlString = "INSERT INTO ".$station -> location."(Name, id, Temperature, Humidity, Pressure, Altitude, Soil) VALUES('".$station -> name."',".$station -> id.",".$station -> temp.",".$station -> hum."," .$station -> press."," .$station -> alt."," .$station -> soil.")";
+    $sqlString = "INSERT INTO ".$station -> location." (Name, id, Temperature, Humidity, Pressure, Altitude, Soil) VALUES('".$station -> name."',".$station -> id.",".$station -> temp.",".$station -> hum."," .$station -> press."," .$station -> alt."," .$station -> soil.")";
+    echo $sqlString;
     if ($conn->query($sqlString) === TRUE) {
         echo "New record created successfully\n";
     } else {
@@ -52,5 +35,40 @@ function insertIntoDb($conn, $station)
 }
 
 
+function initializeStationObj()
+{
+    $station = new stdClass();
+    $station -> location =  -300 ;
+    $station -> name = -300 ;
+    $station -> id =  -300 ;
+    $station -> temp =  -300 ;
+    $station -> hum =  -300 ;
+    $station -> press =  -300 ;
+    $station -> alt =  -300 ;
+    $station -> soil =  -300 ;
+    return $station;
+}
 
+function getValues(&$station)
+{
+    $station -> location = $_GET["loc"];
+    $station -> name = $_GET["name"]; 
+    $station -> id = $_GET["id"];
+    $station -> temp = $_GET["temp"];
+    $station -> hum = $_GET["humidity"];
+    $station -> press = $_GET["press"];
+    $station -> alt = $_GET["altitude"];
+    $station -> soil = $_GET["soil"];
+}
+function fill($station)
+{
+    $station -> location = "patras";
+    $station -> name = "kostas" ;
+    $station -> id = 0;
+    $station -> temp = 40;
+    $station -> hum = 67;
+    $station -> press = 577;
+    $station -> alt = 8;
+    $station -> soil = 0;
+}
 ?>
